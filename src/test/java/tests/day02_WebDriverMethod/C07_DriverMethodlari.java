fd16c0d534812097f89e8b18a1f77b55cf68af50
abdullahyildiz52 <ce_abdullahyildiz@hotmail.com>
2023-06-14 18:58:38 +0300

    C07 sonraya biraklan soru cozuldu

--- src/test/java/tests/day02_WebDriverMethod/C07_DriverMethodlari.java
+++ src/test/java/tests/day02_WebDriverMethod/C07_DriverMethodlari.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class C07_DriverMethodlari {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","src/resources/chromedriver");
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","src/resources/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().window().maximize();
@@ -33,16 +33,35 @@ public class C07_DriverMethodlari {
         }
 
         // 4.https://www.walmart.com/ sayfasina gidin.
+        driver.get("https://www.walmart.com");
 
         //5. Sayfa basliginin “Walmart.com” icerdigini dogrulayin.
+         String expectedIcerik="Walmart.com";
+         String actualIcerik=driver.getTitle();
+
+         if (expectedIcerik.contains(expectedIcerik)){
+             System.out.println("walmart icerik sorgu testi Passed");
+         }else {
+             System.out.println("walmart icerik sorgu testi Failed");
+
+         }
+        Thread.sleep(3000);
 
         //6. Tekrar “facebook” sayfasina donun
+        driver.navigate().back();
+        Thread.sleep(3000);
 
         // 7. Sayfayi yenileyin
+        driver.navigate().refresh();
+        Thread.sleep(3000);
 
         //8. Sayfayi tam sayfa (maximize) yapin
+        driver.manage().window().maximize();
+
 
         //9.Browser’i kapatin
+        Thread.sleep(3000);
+        driver.close();
 
 
     }
