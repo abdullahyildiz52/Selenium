91c9b46022fbf1e476eeda7ee4f844399ddeff15
abdullahyildiz52 <ce_abdullahyildiz@hotmail.com>
2023-06-19 16:14:31 +0300

    xpath locater

---
+++ src/test/java/tests/practise/P3.java
@@ -0,0 +1,22 @@
+package tests.practise;
+
+public class P3 {
+    //Exercise3...
+    //Navigate to  https://testpages.herokuapp.com/styled/index.html
+    //Click on  Calculater under Micro Apps
+    //Type any number in the first input
+    //Type any number in the second input
+    //Click on Calculate
+    //Get the result
+    //Print the result
+    public static void main(String[] args) {
+        //Navigate to  https://testpages.herokuapp.com/styled/index.html
+        //Click on  Calculater under Micro Apps
+        //Type any number in the first input
+        //Type any number in the second input
+        //Click on Calculate
+        //Get the result
+        //Print the result
+
+    }
+}
